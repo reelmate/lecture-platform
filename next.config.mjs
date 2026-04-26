82a3f382e6fba986ef45274504724c6f93eba6f6
reelmate <kiyumtv@gmail.com>
2026-04-27 01:18:01 +0900

    fix: image unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: 'vz-**.b-cdn.net' },
@@ -8,5 +9,4 @@ const nextConfig = {
     ],
   },
 };
-
 export default nextConfig;
